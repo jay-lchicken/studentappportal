18068560b041c26082e4865af04d4a0b76b702b2
Lai Hong Yu <joyjay606@gmail.com>
2025-09-03 12:45:23 +0800

    update input field label and default value for class name in page component

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -44,8 +44,8 @@ export default async function Page() {
                                 </DialogHeader>
                                 <div className="grid gap-4">
                                     <div className="grid gap-3">
-                                        <Label htmlFor="name-1">Name</Label>
-                                        <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+                                        <Label htmlFor="name-1">Class Name</Label>
+                                        <Input id="name-1" name="name" defaultValue="" />
                                     </div>
 
                                 </div>
